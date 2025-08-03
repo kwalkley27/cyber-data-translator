8c6e382c8d653f85f317e9d850b8d8d892f1cc5b
Kyle Walkley <kylewalkley@gmail.com>
2025-08-03 14:30:13 -0400

    add read file to utils

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_text_from_url(url):
+def get_text_from_url(url:str) -> str:
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
     return soup.get_text(separator="\n", strip=True)
 
+def get_text_from_file(path:str) -> str:
+    with open(path) as file:
+        return file.read()
