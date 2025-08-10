ca26a94e18f02aaa4ab2418c2c5faaf8e0ca354c
Kyle Walkley <kylewalkley@gmail.com>
2025-08-09 20:00:12 -0400

    add dislaimer statement

--- src/main.py
+++ src/main.py
@@ -12,12 +12,19 @@ def define_cli_arguments():
 
     return parser
 
+def print_disclaimer():
+    disclaimer = '''This content was generated using artificial intelligence. While we strive for accuracy, AI-generated content may contain errors or inaccuracies. The output of this model may vary from one request to another. We strongly recommend that you independently verify all information before making decisions. We disclaim any and all liability for any errors or omissions in the content produced by this technology. '''
+
+    print(disclaimer)
+
 def main():
     args = define_cli_arguments().parse_args()
 
     sample_text = utils.get_text_from_file(args.sample)
     agent = translator_factory.get_agent(AGENT)()
     schema_translator = translator_factory.get_schema(args.schema)(agent)
+
+    print_disclaimer()
 
     schema_translator.translate(sample_text)
 
